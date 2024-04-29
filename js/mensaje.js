3c3fa611b4e6d1c0f55e3d816a1c4bc3e61dca44
Kiben1 <147743587+Kiben1@users.noreply.github.com>
2024-04-29 13:56:15 -0400

    comit

---
+++ js/mensaje.js
@@ -0,0 +1,40 @@
+//mi primer script  ...Math.PI
+//console.log("hola a todos")
+//alert("este es mi primer script")
+document.write("<h1>saludo a todos</h1>")
+
+/*var nombre="Kiben"
+let edad= 22
+const PI=3.14
+let x=5;
+//x +=2;
+//x-=4;
+x*=2;
+//x/=4;
+//x%=2;
+if(x==2){
+    document.write("<h1>VERDA</h1>")
+}else {
+    document.write("<h1> FALSO:"+x+"</h1>")
+}
+*/
+//mi segundo scrip
+/*edad.promat("edad DEL PERRO")
+r=Math.log(edad)
+dc*16r+31
+*/
+//INGRESE DATO A 
+//INGRESE DATO B
+let a=prompt("intro a")
+let b=prompt("intro b")
+ar=a**2
+br=b**2
+c=ar+br
+const PI=3.14
+F=1/2
+d=c*F
+
+R=PI*2
+s=Math.pow(R,d)
+
+document.write("<h1>"+d+"</h1>")
